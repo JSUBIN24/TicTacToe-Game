91642f2c8b9004b563cb721ba29046dd3a020a0e
subin <subinbenat@gmail.com>
2025-08-22 00:28:36 +0200

    feat(game response): add centralized method to build the response

--- src/main/java/com/adsquare/tictactoe/dto/GameResponse.java
+++ src/main/java/com/adsquare/tictactoe/dto/GameResponse.java
@@ -1,9 +1,13 @@
 package com.adsquare.tictactoe.dto;
 
+import com.adsquare.tictactoe.model.Game;
 import com.adsquare.tictactoe.model.GameStatus;
 import com.adsquare.tictactoe.model.Player;
 
 import java.util.UUID;
 
 public record GameResponse(UUID gameId, String board, Player nextPlayer, GameStatus gameStatus) {
+    public static GameResponse buildGameResponse (Game game) {
+        return new GameResponse(game.getId(), game.getBoard(),game.getNextPlayer(),game.getStatus());
+    }
 }
